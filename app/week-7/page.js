f6eb88cbe293355efc7dcfde6a0470059feb7c4c
BrandtFgit <Brandt.fowler@edu.sait.ca>
2024-03-10 13:14:15 -0600

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -38,12 +38,17 @@ export default function Page() {
     };
 
     return (
-        <main className="bg-slate-950 p-2 m-2">
-            <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-            <h3 className="text-xl font-bold">Add New Item</h3>
-            <NewItem onAddItem={handleAddItem}></NewItem>
-            <ItemList items={items} onItemSelect={handleItemSelect}></ItemList>
-            <MealIdeas ingredient={selectedItemName} />
+        <main className="bg-slate-950 p-2 m-1 flex">
+            <div>
+              <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+              <h3 className="text-xl font-bold">Add New Item</h3>
+              
+              <NewItem onAddItem={handleAddItem}></NewItem>
+              <ItemList items={items} onItemSelect={handleItemSelect}></ItemList>
+            </div>
+            <div className="flex-1 ml-4">
+              <MealIdeas ingredient={selectedItemName} />
+            </div>
         </main>
     );
 }
