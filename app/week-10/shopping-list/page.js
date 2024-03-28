06a44b5cdc495f4372fc296c7d7f9ec497da48ac
BrandtFgit <Brandt.fowler@edu.sait.ca>
2024-03-28 17:08:20 -0600

    wip week 10

---
+++ app/week-10/shopping-list/page.js
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Item from "./item";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from './items.json';
+import MealIdeas from "./meal-ideas"; 
+import { useUserAuth } from "../_utils/auth-context.js";
+
+function clean(text) {
+  // Remove emojis
+  text = removeEmojis(text);
+  
+  // Remove amounts like "4 L" or "dozen"
+  text = text.replace(/\d+(\s?L|dozen|kg)?/, '').trim();
+  text = text.split(',')[0].trim();
+  return text;
+}
+
+function removeEmojis(text) {
+  return text.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{1F966}]/gu, '');
+}
+
+export default function Page() {
+    const [items, setItems] = useState(itemsData);
+    const [selectedItemName, setSelectedItemName] = useState("");
+    const {user} = useUserAuth();
+    console.log(user);
+    // Check if the user is not logged in (user object is null)
+    if (!user) {
+      // Redirect the user to the landing page if desired
+      // You can use React Router or any other routing mechanism
+      // Example using React Router: 
+      // return <Redirect to="/" />;
+      return <p>You need to log in to access the shopping list.</p>;
+    }
+
+
+    const handleAddItem = (newItem) => {
+        setItems(prevItems => [...prevItems, newItem]);
+    };
+
+
+    const handleItemSelect = (item) => {
+
+      const cleanedName = clean(item.name.trim());
+      console.log(cleanedName);
+      setSelectedItemName(cleanedName);
+    };
+
+    return (
+        <main className="bg-slate-950 p-2 m-1 flex">
+            <div>
+              <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+              <h3 className="text-xl font-bold">Add New Item</h3>
+              
+              <NewItem onAddItem={handleAddItem}></NewItem>
+              <ItemList items={items} onItemSelect={handleItemSelect}></ItemList>
+            </div>
+            <div className="flex-1 ml-4">
+              <MealIdeas ingredient={selectedItemName} />
+            </div>
+        </main>
+    );
+}
